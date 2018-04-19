3e892fd563487bf5a3f5f2f9e334e01044472ca7
leeshun <leeshun1230@gmail.com>
2018-04-20 00:14:24 +0800

    add listview layout

--- app/src/main/java/sse/bupt/cn/translator/activity/MainActivity.java
+++ app/src/main/java/sse/bupt/cn/translator/activity/MainActivity.java
@@ -6,15 +6,19 @@ import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import sse.bupt.cn.translator.R;
+import sse.bupt.cn.translator.adapter.MenuTextAdapter;
 import sse.bupt.cn.translator.constants.MessageType;
 import sse.bupt.cn.translator.constants.UrlConstant;
 import sse.bupt.cn.translator.model.MenuItem;
 import sse.bupt.cn.translator.network.StringRequestWrapper;
 import sse.bupt.cn.translator.responsehandler.MenuItemHandler;
+import sse.bupt.cn.translator.util.MenuPreferenceReader;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -25,14 +29,60 @@ public class MainActivity extends AppCompatActivity {
 
     private MenuItemHandler itemHandler;
 
+    private MenuPreferenceReader preferenceReader;
+
+    private List<MenuItem> items;
+
+    private MenuTextAdapter adapter;
+
+    private ListView menuview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initializeHandler();
-        itemHandler = new MenuItemHandler(handler);
-        requestHandler = new StringRequestWrapper(this);
+
+        initialize();
+
         requestHandler.sendGetRequest(UrlConstant.GETMENUS, itemHandler);
+    }
+
+    private void initialize() {
+        setContentView(R.layout.activity_main);
+
+        initializeHandler();
+
+        items = new ArrayList<>();
+
+        initializeListView();
+
+        itemHandler = new MenuItemHandler(handler);
+
+        preferenceReader = new MenuPreferenceReader(this);
+
+        List<MenuItem> preferemceItems = preferenceReader.getItems();
+
+        //get items from the internet
+        if (preferemceItems == null || preferemceItems.isEmpty()) {
+            requestHandler = new StringRequestWrapper(this);
+        }
+        //clear previous items
+        int size = items.size();
+        for (int index = 0; index < size; index++) {
+            items.remove(index);
+        }
+        items.addAll(preferemceItems);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initializeListView() {
+        menuview = findViewById(R.id.menu_list_view);
+        initializeAdapter();
+        menuview.setAdapter(adapter);
+    }
+
+
+    private void initializeAdapter() {
+        adapter = new MenuTextAdapter(items, this);
     }
 
 
@@ -43,7 +93,14 @@ public class MainActivity extends AppCompatActivity {
                 int messageType = msg.what;
                 switch (messageType) {
                     case MessageType.MENU_RESPONSE_SUCCESS:
-                        List<MenuItem> items = (List<MenuItem>) msg.obj;
+                        List<MenuItem> tmpItems = (List<MenuItem>) msg.obj;
+                        //clear previous list
+                        for (int index = 0; index < items.size(); index++) {
+                            items.remove(index);
+                        }
+
+                        items.addAll(tmpItems);
+                        adapter.notifyDataSetChanged();
                         for (MenuItem item : items) {
                             Log.i(TAG, "---" + item.toString() + "---");
                         }
@@ -58,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
                         Log.i(TAG, "menu request internet error");
                         break;
                 }
-                super.handleMessage(msg);
             }
         };
     }
