ba23347d3d52dda5c9f1e5b7eb4e9240020dbfbb
shun lee <leeshun1230@gmail.com>
2018-04-20 23:28:40 +0800

    add text layout

--- app/src/main/java/sse/bupt/cn/translator/activity/TextActivity.java
+++ app/src/main/java/sse/bupt/cn/translator/activity/TextActivity.java
@@ -1,16 +1,57 @@
 package sse.bupt.cn.translator.activity;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 import sse.bupt.cn.translator.R;
+import sse.bupt.cn.translator.model.Text;
+import sse.bupt.cn.translator.util.TextFileWriter;
 
 public class TextActivity extends AppCompatActivity {
     private static final String TAG = "TextActivity";
+
+    private List<Text> texts;
+
+    private String path;
+
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
+        initialize();
     }
+
+
+    public void initialize() {
+        initializeHandler();
+    }
+
+    private void initializeHandler() {
+        handler = new Handler(Looper.myLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                int type = msg.what;
+                switch (type) {
+
+                }
+                super.handleMessage(msg);
+            }
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        TextFileWriter writer = new TextFileWriter(path,texts,handler);
+        writer.start();
+    }
+
+
 }
